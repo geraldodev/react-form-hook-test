2bd241d52b00822be86b7fe1c211b467a641ae75
geraldo lopes de souza <geraldo.lopes@ahsr.org.br>
2020-04-20 17:43:28 -0300

    validation with message on javascript, ExampleClojure barely started

--- src/todo_mvc/core.cljs
+++ src/todo_mvc/core.cljs
@@ -11,8 +11,7 @@
     ["react-router-dom" :as rr]
     ["react-hook-form" :as rhf]))
 
-
-(defnc App
+(defnc ExampleClojure
   []
   (j/let [[state set-state] (hooks/use-state {} )
           ^:js {:keys [register
@@ -23,41 +22,146 @@
                        getValues] :as obj} (rhf/useForm)
 
           ;; https://react-hook-form.com/api/#handleSubmit
-          fn-submit (fn [data e]
+          ;; this is just called if the form has no error
+          fn-submit (fn [data event]
                       (.log js/console "data on handleSubmit")
                       (.log js/console data)
+
                       (.log js/console "getValues on handleSubmit")
-                      (.log js/console (getValues)))]
+                      (.log js/console (getValues))
+                      )]
 
-    (.log js/console obj)
-    (d/form 
-      {:on-submit (fn [e]
-                    (.log js/console "on-submit")
-                    (handleSubmit fn-submit)
-                    (.preventDefault e)
-                    (.log js/console "errors:")
-                    (.log js/console errors)
-                    (.log js/console "getValues():")
-                    (.log js/console (getValues))
-                    )}
-      (d/div
-        (.createElement r "input"
-                        #js {:ref (register #js {:name "foo"}
-                                            #js {:required true})
-                             :name "foo"
-                             :defaultValue "hey"
-                             :onChange 
-                             (fn [e]
-                               (let [v (->  e .-target .-value)]
-                                 (.log js/console (str "setting value " v))
-                                 (setValue "foo" v)) )}))
-      (d/div 
-        (d/input {:name "teste" 
-                  :ref (register #js {:required true})}))
-      (d/div
-        (d/button {:type "submit"} "Submit"))
-        )))
+    (d/div
+      (d/h3 "Form data on edn")
 
+      (d/form
+        {:on-submit (handleSubmit fn-submit)}
+
+        (d/div
+          (d/label "person_name")
+          (d/input {; option 1
+                    ; setting input's :name and calling register on :ref are enough to
+                    ; to register the input with react-form-hook
+                    ; like this :
+                    ; :name "person_name"
+                    ; :ref (register #js {:required "person's name is required"})
+
+                    ; option 2
+                    ; the alternative is specify the name on the register's call
+                    ; and invoke setValue in a on-change handler
+                    :ref (register "person_name" ; <- notice the name of the field
+                                   #js {:required "person's name is required"})
+                    :on-change
+                    (fn [e]
+                      (let [v (->  e .-target .-value)]
+                        (.log js/console (str "setting value " v))
+                        (setValue "person_name" v)) )
+                    })
+          ($ rhf/ErrorMessage {:name "person_name" :errors errors}))
+
+        (d/div
+          (d/label "sex")
+          (d/input {:name "sex"
+                    :ref (register
+                           #js {:validate
+                                #js {:male_female
+                                     ;; we have to coerce the validation because a string result is considered an error message, by coercing when returning true it is considered a valid value
+                                     (fn [v] (or (boolean
+                                                   (#{"male" "female"} v ))
+                                                 "Should be male or female"))}
+                                })})
+          ($ rhf/ErrorMessage {:name "sex" :errors errors}) )
+
+        (d/div
+          (d/button {:type "submit"} "Submit"))
+
+        (d/div (d/p "see console for errors object"))
+        (.log js/console "errors")
+        (.log js/console errors)
+
+        (d/div
+          (d/p "Values")
+          (d/div (.stringify js/JSON (getValues))))
+        ))))
+(defnc ExampleJavascript
+  []
+  (j/let [[state set-state] (hooks/use-state {} )
+          ^:js {:keys [register
+                       handleSubmit
+                       watch
+                       errors
+                       setValue
+                       getValues] :as obj} (rhf/useForm)
+
+          ;; https://react-hook-form.com/api/#handleSubmit
+          ;; this is just called if the form has no error
+          fn-submit (fn [data event]
+                      (.log js/console "data on handleSubmit")
+                      (.log js/console data)
+
+                      (.log js/console "getValues on handleSubmit")
+                      (.log js/console (getValues))
+                      )]
+
+    (d/div
+      (d/h3 "Form data on javascript Object")
+
+      (d/form
+        {:on-submit (handleSubmit fn-submit)}
+
+        (d/div
+          (d/label "person_name")
+          (d/input {; option 1
+                    ; setting input's :name and calling register on :ref are enough to
+                    ; to register the input with react-form-hook
+                    ; like this :
+                    ; :name "person_name"
+                    ; :ref (register #js {:required "person's name is required"})
+
+                    ; option 2
+                    ; the alternative is specify the name on the register's call
+                    ; and invoke setValue in a on-change handler
+                    :ref (register "person_name" ; <- notice the name of the field
+                                   #js {:required "person's name is required"})
+                    :on-change
+                    (fn [e]
+                      (let [v (->  e .-target .-value)]
+                        (.log js/console (str "setting value " v))
+                        (setValue "person_name" v)) )
+                    })
+          ($ rhf/ErrorMessage {:name "person_name" :errors errors}))
+
+        (d/div
+          (d/label "sex")
+          (d/input {:name "sex"
+                    :ref (register
+                           #js {:validate
+                                #js {:male_female
+                                     ;; we have to coerce the validation because a string result is considered an error message, by coercing when returning true it is considered a valid value
+                                     (fn [v] (or (boolean
+                                                   (#{"male" "female"} v ))
+                                                 "Should be male or female"))}
+                                })})
+          ($ rhf/ErrorMessage {:name "sex" :errors errors}) )
+
+        (d/div
+          (d/button {:type "submit"} "Submit"))
+
+        (d/div (d/p "see console for errors object"))
+        (.log js/console "errors")
+        (.log js/console errors)
+
+        (d/div
+          (d/p "Values")
+          (d/div (.stringify js/JSON (getValues))))
+        ))))
+
+(defn App
+  []
+  (d/div 
+    ($ ExampleClojure)
+    ($ ExampleJavascript))
+  )
 
 (defn ^:export start
   []
